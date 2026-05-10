7c436c2f755b3a06bb3ab80525cf44165e33ee6b
Anjaliydv9598 <anjaliydv9598.com>
2026-05-10 21:40:50 +0530

    Added PrintBasicDetails program

---
+++ PrintBasicDetails.java
@@ -0,0 +1,19 @@
+package com.Number.Program.Comment;
+
+public class PrintBasicDetails {
+
+	// WAJP to Print Basic Details
+	public static void main(String[] args) {
+		
+		String name="Anjali";
+		int id = 122;
+		String phone_no = "1234567890";	// Phone number is stored as String to avoid precision issues and allow formatting
+		
+		
+		System.out.println("Name : " +name );
+		System.out.println("Id : " +id);
+		System.out.println("Contact Number : " + phone_no);
+
+	}
+
+}
