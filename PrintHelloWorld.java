2f7b40bc45fefbb5d4fa37d86572aa866978fa28
Anjaliydv9598 <anjaliydv9598.com>
2026-05-10 21:45:46 +0530

    WAJP to Print Hello World

---
+++ PrintHelloWorld.java
@@ -0,0 +1,12 @@
+package com.Number.Program.Comment;
+
+public class PrintHelloWorld {
+
+	//WAJP to Print Hllo World
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
