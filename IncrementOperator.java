dcb8bf76366c746638c4a24ab7f7d532f3c8e69b
Anjaliydv9598 <anjaliydv9598.com>
2026-05-10 22:02:51 +0530

    WAJP for increment operator Practice

---
+++ IncrementOperator.java
@@ -0,0 +1,15 @@
+package com.Number.Program.Comment;
+
+public class IncrementOperator {
+
+	public static void main(String[] args) {
+		int n = 10;
+		n=n+1;			// n=10+1 =11
+		n+=1;			// n=n+1 =12
+		n++;			//Post-increment (increase by 1) =13
+		++n;			//Pre-increment (also increase by 1) =14
+		System.out.println(n);
+
+	}
+
+}
