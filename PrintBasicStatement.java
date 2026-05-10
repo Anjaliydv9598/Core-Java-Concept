e07dae29c9be221a7d3ba0e17d5322eb131dffe3
Anjaliydv9598 <anjaliydv9598.com>
2026-05-10 21:55:47 +0530

    WAJP Print Basic Statement

---
+++ PrintBasicStatement.java
@@ -0,0 +1,14 @@
+package com.Number.Program.Comment;
+
+public class PrintBasicStatement {
+
+	//WAJP Print Basic Statement
+	public static void main(String[] args) {
+		
+		System.out.println("Hello");
+		System.out.print("Good Morning");
+		
+
+	}
+
+}
