a88539fd9024ed06ac8d9b6cab2699f5f842cc99
Anjaliydv9598 <anjaliydv9598.com>
2026-05-17 11:28:24 +0530

    WAJP that the person is eligible to vote

---
+++ RelationalOperator.java
@@ -0,0 +1,22 @@
+package com.program.basic;
+
+public class RelationalOperator {
+
+	// WAJP "the person is eligible to vote"
+	
+	public static void main(String[] args) {
+		
+		int age = 17 ;
+		
+		if(age>=18) {
+			System.out.println(age + "Yes , The Person is eligible for vote !");
+		}
+		else {
+			System.out.println("OOPS, The Person is not elegible for vote !");
+		}
+		
+		
+
+	}
+
+}
