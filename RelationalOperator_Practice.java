92be8958313e438d6ca2d480e4f046bc5ebff820
Anjaliydv9598 <anjaliydv9598.com>
2026-05-17 11:51:33 +0530

    WAJP that the Number is Positive

---
+++ RelationalOperator_Practice.java
@@ -0,0 +1,17 @@
+package com.program.basic;
+
+public class RelationalOperator_Practice
+{
+	//WAJP that the Number is Positive
+	
+	public static void main(String [] args) {
+	
+		int number = 13;
+		
+	if(number >= 0) {
+			System.out.println("The given number is positive ");
+		}else {
+			System.out.println("The given number is not positive ");
+		}
+	}
+}
